2f0d2e6e0f9cd26adf0057ab14af22aff60f7186
JOEFFREY EDRIAN CARANI <2240191@slu.edu.ph>
2025-11-02 23:12:21 +0800

    Implemented ant run.single and added javadocs comments again

--- src/solutions/array/PascalTriangle.java
+++ src/solutions/array/PascalTriangle.java
@@ -7,10 +7,33 @@ import java.util.List;
 
 
 /**
- * TODO
- * Link: https://leetcode.com/problems/pascals-triangle/description
+ * The {@code PascalTriangle} class generates Pascal's Triangle up to a given number of rows.
+ * <p>
+ * Each row in Pascal's Triangle represents the coefficients of binomial expansion.
+ * For example:
+ * <pre>
+ * Row 1: [1]
+ * Row 2: [1, 1]
+ * Row 3: [1, 2, 1]
+ * Row 4: [1, 3, 3, 1]
+ * Row 5: [1, 4, 6, 4, 1]
+ * </pre>
+ * <p>
+ * This class implements the {@link solutions.Solution} interface, which requires a {@code run()} method
+ * to demonstrate the functionality.
+ *
+ * <p><b>Problem Link:</b>
+ * <a href="https://leetcode.com/problems/pascals-triangle/description">
+ * https://leetcode.com/problems/pascals-triangle/description</a>
+ *
  */
-public class PascalTriangle implements Solution {
+
+public class PascalTriangle implements Solution {/**
+     * Generates Pascal's Triangle up to the specified number of rows.
+     *
+     * @param numRows the number of rows to generate; must be greater than 0
+     * @return a list of lists representing Pascal's Triangle, where each inner list is a row
+     */
     public List<List<Integer>> generate(int numRows) {
         List <List<Integer>> pascalTriangle = new ArrayList<>();
 
@@ -53,7 +76,20 @@ public class PascalTriangle implements Solution {
         // Rerturn the entire Pascal's Triangle
         return pascalTriangle;
     }
-
+    /**
+     * Demonstrates the {@link #generate(int)} method by printing
+     * Pascal's Triangle with a fixed number of rows.
+     * <p>
+     * Example output:
+     * <pre>
+     * Pascal's Triangle with 5 rows:
+     * [1]
+     * [1, 1]
+     * [1, 2, 1]
+     * [1, 3, 3, 1]
+     * [1, 4, 6, 4, 1]
+     * </pre>
+     */
     @Override
     public void run() {
         // Example test case
--- src/solutions/array/RemoveDuplicatesFromSortedArray.java
+++ src/solutions/array/RemoveDuplicatesFromSortedArray.java
@@ -3,10 +3,34 @@ package solutions.array;
 import solutions.Solution;
 
 /**
- * TODO
- * Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description
+ * Solution for the "Remove Duplicates from Sorted Array" problem.
+ * <p>
+ * This class removes duplicate elements from a sorted integer array in-place.
+ * After processing, the array will contain only unique elements at the beginning,
+ * and the method will return the count of these unique elements.
+ * </p>
+ *
+ * <p><b>Example:</b></p>
+ * <pre>
+ * Input:  nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: new length = 5
+ * Modified array = [0,1,2,3,4,...]
+ * </pre>
+ *
+ * <p><b>Problem Link:</b> <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array/description">
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description</a></p>
  */
 public class RemoveDuplicatesFromSortedArray implements Solution {
+    /**
+     * Removes duplicate values from a sorted integer array.
+     * <p>
+     * The function modifies the input array in-place such that
+     * the first part of the array contains only unique values.
+     * </p>
+     *
+     * @param nums the sorted array of integers
+     * @return the number of unique elements in the array
+     */
     public int removeDuplicates(int[] nums) {
         if(nums.length == 0) return 0;
         int i = 0;
@@ -21,6 +45,10 @@ public class RemoveDuplicatesFromSortedArray implements Solution {
         return i + 1;
     }
 
+    /**
+     * Runs a sample test case demonstrating the
+     * {@link #removeDuplicates(int[])} method.
+     */
     @Override
     public void run() {
         // Example test case
@@ -39,6 +67,11 @@ public class RemoveDuplicatesFromSortedArray implements Solution {
         System.out.println("\nNew length: " + newLength);
     }
 
+    /**
+     * Utility method to print all elements in an array.
+     *
+     * @param arr the array to print
+     */
     private void printArray(int[] arr) {
         for (int num : arr) {
             System.out.print(num + " ");
